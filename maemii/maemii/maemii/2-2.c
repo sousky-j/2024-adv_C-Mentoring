4cb7ec58717457c12bf1f000b7701b6c04883bcb
maemii <kimdy1064@gmail.com>
2024-07-15 19:38:31 +0900

    huhhuh

---
+++ maemii/maemii/maemii/2-2.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void sort_arr(char** arr, int cnt) {
+	char* tmp;
+	for (int i = 0; i < cnt - 1; i++) {
+		for (int j = i + 1; j < cnt; j++) {
+
+			if (strcmp(arr[i], arr[j]) > 0) {
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+	for (int i = 0; i < cnt; i++)
+		printf("%s\n", arr[i]);
+}
+
+int main() {
+	int cnt = 0;
+	char input[101];
+	char** arr = (char*)malloc(sizeof(char*));
+
+	while (1) {
+		gets_s(input, 101);
+		if (input[0] == '\0')
+			break;
+
+		arr = (char**)realloc(arr, sizeof(char*) * (cnt + 1));
+		arr[cnt] = (char*)malloc(sizeof(char) * (sizeof(input) + 1));
+
+		strcpy(arr[cnt], input);
+		cnt++;
+	}
+
+	sort_arr(arr, cnt);
+
+	for (int i = 0; i < cnt; i++)
+		free(arr[i]);
+	free(arr);
+
+	return 0;
+}
