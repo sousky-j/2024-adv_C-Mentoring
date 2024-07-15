c6cef8816a7093a007c4c572c30589ad3f7dce94
holychan <east0321@outlook.kr>
2024-07-15 18:25:53 +0900

    hotfix: 출력 형식 및 오류 수정

--- Seongchan/project/2-1.c
+++ Seongchan/project/2-1.c
@@ -16,12 +16,19 @@ int main(){
     int input = 235;
     // 입력
     scanf("%d", &input);
+    int save = input;
 
     char *result = (char *)malloc(sizeof(char));
+    int *result_num = (int *)malloc(sizeof(int));
     int result_size = 0;
+    int result_num_size = 0;
 
-    for(int i = 0; i < 12; i++){
+    for(int i = 0; i <= 12; i++){
+
+        int count = 0;
+
         while(input >= nums[i]){
+            count += 1;
             // 수 차감
             input -= nums[i];
 
@@ -30,16 +37,34 @@ int main(){
 
             // 메모리 확보: realloc -> 조건 만족
             result = (char *)realloc(result, result_size + string_len);
+
             // 문자 이어붙이기
             strcat(result, string[i]);
 
             result_size += string_len;
 
         }
+        if(count){
+            result_num_size++;
+            result_num = (int *)realloc(result_num, sizeof(int) * result_num_size);
+            result_num[result_num_size - 1] = nums[i] * count;
+        }
+
+    }
+
+    printf("%d = ", save);
+
+    for(int i = 0; i < result_num_size; i++){
+        if(i + 1 != result_num_size){
+            printf("%d + ", result_num[i]);
+            continue;
+        }
+
+        printf("%d = ", result_num[i]);
     }
 
     printf("%s", result);
-
+    free(result_num);
     free(result);
 
 
