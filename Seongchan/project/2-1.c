326b0d50b4e084248cc40b62755231e1b521bbe9
holychan <east0321@outlook.kr>
2024-07-16 15:29:16 +0900

    hotfix: 출력 형식 2차 수정

--- Seongchan/project/2-1.c
+++ Seongchan/project/2-1.c
@@ -19,16 +19,12 @@ int main(){
     int save = input;
 
     char *result = (char *)malloc(sizeof(char));
-    int *result_num = (int *)malloc(sizeof(int));
     int result_size = 0;
-    int result_num_size = 0;
 
     for(int i = 0; i <= 12; i++){
 
-        int count = 0;
-
         while(input >= nums[i]){
-            count += 1;
+
             // 수 차감
             input -= nums[i];
 
@@ -36,35 +32,34 @@ int main(){
             int string_len = strlen(string[i]);
 
             // 메모리 확보: realloc -> 조건 만족
-            result = (char *)realloc(result, result_size + string_len);
+            result_size += string_len;
+            result = (char *)realloc(result, result_size);
 
             // 문자 이어붙이기
             strcat(result, string[i]);
-
-            result_size += string_len;
-
-        }
-        if(count){
-            result_num_size++;
-            result_num = (int *)realloc(result_num, sizeof(int) * result_num_size);
-            result_num[result_num_size - 1] = nums[i] * count;
         }
 
     }
 
     printf("%d = ", save);
 
-    for(int i = 0; i < result_num_size; i++){
-        if(i + 1 != result_num_size){
-            printf("%d + ", result_num[i]);
-            continue;
-        }
-
-        printf("%d = ", result_num[i]);
+    // 출력
+    if(save >= 100){
+        printf("%d ", save/100 * 100);
+        save %= 100;
     }
+    if(save != 0) printf("+ ");
+    if(save >= 10){
+        printf("%d ", save/10 * 10);
+        save %= 10;
+    }
+    if(save != 0) printf("+ ");
+    if(save >= 1){
+        printf("%d ", save/1);
+    }
+    printf("= ");
 
-    printf("%s", result);
-    free(result_num);
+    printf("%s\n", result);
     free(result);
 
 
